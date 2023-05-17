57b60049a488b02bb352b98a83406bc162b6cbd6
syedhamzaalinaqvi <107138453+syedhamzaalinaqvi@users.noreply.github.com>
2023-05-17 01:53:29 -0700

    main.js javscript file add to all

---
+++ main.js
@@ -0,0 +1,48 @@
+
+   
+   
+   /*--section start-- */
+     
+   var bollywoodsec = document.getElementById('pagebollywood');    
+   console.log("bollywood section :" + bollywoodsec);
+    
+    /*--section  end-- */
+   
+
+/*----------------------Movile Nav----------------------------------------------*/
+
+document.getElementById("fa-bars").addEventListener("click", function(){ 
+  document.getElementById("mobilenav").style.marginLeft="0%"
+  document.getElementById("mobilenav").style.transition="0.5s%"
+
+})
+  
+document.getElementById("closebar").addEventListener("click", function(){
+  document.getElementById("mobilenav").style.marginLeft ="-100%"
+  document.getElementById("mobilenav").style.transition ="1.5s"
+
+});
+/*--Movile Nav End*/
+
+
+
+
+/* Slide show Start*/
+
+                    var slides = document.querySelectorAll('.slide');
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide, 2000);
+
+function nextSlide() {
+  slides[currentSlide].classList.remove('active');
+  currentSlide = (currentSlide + 1) % slides.length;
+  slides[currentSlide].classList.add('active');
+}
+/* Slide show END*/
+
+let noteclose = document.getElementById("noteclose");
+noteclose.addEventListener('click',()=>{
+document.querySelector("#notediv").style.display="none";
+
+
+})
