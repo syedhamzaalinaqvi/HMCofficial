ce759141e60a94b62300efb605e855e3790c2a7a
syedhamzaalinaqvi <107138453+syedhamzaalinaqvi@users.noreply.github.com>
2023-10-16 13:38:43 +0500

    offline setting server container + css and js added

--- main.js
+++ main.js
@@ -1,28 +1,28 @@
 
-   
-   
-   /*--section start-- 
 
-      var punjabi_section = document.querySelector("#pagepunjabi").addEventListener('click', function(){
-         classList.add('active-section');
 
-   });
-   
-   
-    /*--section  end-- */
-   
+/*--section start-- 
+
+   var punjabi_section = document.querySelector("#pagepunjabi").addEventListener('click', function(){
+      classList.add('active-section');
+
+});
+ 
+ 
+ /*--section  end-- */
+
 
 /*----------------------Movile Nav----------------------------------------------*/
 
-document.getElementById("fa-bars").addEventListener("click", function(){ 
-  document.getElementById("mobilenav").style.marginLeft="0%"
-  document.getElementById("mobilenav").style.transition="0.5s%"
+document.getElementById("fa-bars").addEventListener("click", function () {
+  document.getElementById("mobilenav").style.marginLeft = "0%"
+  document.getElementById("mobilenav").style.transition = "0.5s%"
 
 })
-  
-document.getElementById("closebar").addEventListener("click", function(){
-  document.getElementById("mobilenav").style.marginLeft ="-100%"
-  document.getElementById("mobilenav").style.transition ="1.5s"
+
+document.getElementById("closebar").addEventListener("click", function () {
+  document.getElementById("mobilenav").style.marginLeft = "-100%"
+  document.getElementById("mobilenav").style.transition = "1.5s"
 
 });
 //--Movile Nav End
@@ -32,7 +32,7 @@ document.getElementById("closebar").addEventListener("click", function(){
 
 // Slide show Start
 
-                    var slides = document.querySelectorAll('.slide');
+var slides = document.querySelectorAll('.slide');
 var currentSlide = 0;
 var slideInterval = setInterval(nextSlide, 2000);
 
@@ -44,8 +44,8 @@ function nextSlide() {
 // Slide show END
 
 let noteclose = document.getElementById("noteclose");
-noteclose.addEventListener('click',()=>{
-document.querySelector("#notediv").style.display="none";
+noteclose.addEventListener('click', () => {
+  document.querySelector("#notediv").style.display = "none";
 })
 
 
@@ -59,56 +59,68 @@ document.querySelector("#notediv").style.display="none";
 
 var filter = document.getElementById('myInput');
 
-filter.addEventListener('blur',()=>{
-  filter.style.border='1px solid blue';
-  
+filter.addEventListener('blur', () => {
+  filter.style.border = '1px solid blue';
+
 })
-filter.addEventListener('focus',()=>{
-  filter.style.border='1px solid green';
-  
+filter.addEventListener('focus', () => {
+  filter.style.border = '1px solid green';
+
 })
 
 
- filter.addEventListener('keyup', () =>{
+filter.addEventListener('keyup', () => {
 
 
-
-
-
-
-let searchbar  = filter.value.toUpperCase();
-let Allmovies = document.getElementById('all_movies');
-let movie1= Allmovies.getElementsByClassName('movie1');
-
-for(var i = 0; i < movie1.length ; i++ ){
-
-
-
-let link= movie1[i].getElementsByTagName('a')[0];
-
+  function search(){
+    var searchitem= document.getElementById('txt').value ;
+    localStorage.setItem("textvalue", searchitem);
+    return false
   
-  let textvalue =  link.textContent || link.innerText ;   
-   
-  if(textvalue.toUpperCase().indexOf(searchbar) > -1 ){
-    movie1[i].style.display= "";
 
-     
-   }else{
-    movie1[i].style.display="none";
-   }
- }
-  })
+
+
+  let searchbar = filter.value.toUpperCase();
+  let Allmovies = document.getElementById('all_movies');
+  let movie1 = Allmovies.getElementsByClassName('movie1');
+
+  for (var i = 0; i < movie1.length; i++) {
+    let link = movie1[i].getElementsByTagName('a')[0];
+    let textvalue = link.textContent || link.innerText;
+    if (textvalue.toUpperCase().indexOf(searchbar) > -1) {
+      movie1[i].style.display = "";
+    } else {
+      movie1[i].style.display = "none";
+    }
+  }}
+})
+
 //==========Search Filter JS End=====================
 
-//==========Search bar removed start=====================
-let searchbox = document.getElementsByClassName("searchbox");
- searchbox.style.display="none";
-//==========Search bar removed End=====================
-
 //==========add adsterra link  start=====================
-var head = document.getElementsByTagName('head');
-
+/*var head = document.getElementsByTagName('head');
 var header = head.classList.add("header");
-
-
+*/
 //==========add adsterra link End=====================
+
+
+//============= Downloading Container ===============
+
+/*
+var go_to_download = document.getElementById("go-to-download");
+const downloading_container = document.getElementById("d-container");
+go_to_download.addEventListener("click", () => {
+  downloading_container.style.display = "block";
+});
+const close_downloading_container = document.getElementById("close-downloading-container").addEventListener("click", () => {
+  downloading_container.style.display = "none";
+
+});
+*/
+
+
+
+
+
+
+
